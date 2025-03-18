64e6da6961c03bbc014c8d60c0a441a2229898a5
RamezHany <rramezhany@gmail.com>
2025-03-18 11:55:33 +0200

    ioio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
